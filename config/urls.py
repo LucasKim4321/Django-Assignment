9cf12c4e1da6a38aaa85b3e6725404475cc80cd3
LucasKim4321 <xowls0131@naver.com>
2025-03-22 00:46:46 +0900

    ✨feat: chapter_07 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
@@ -15,4 +17,9 @@ urlpatterns = [
     path('accounts/signup/', user_views.sign_up, name='signup'),
     # CBV URL include
     path('cbv/', include('todo.urls')),
+    # summernote URL include
+    path('summernote/', include('django_summernote.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
